06e078f651162ee274ae35e765518e12ff333716
Ryan Shin <1982sdr@hanmail.net>
2018-04-20 07:09:55 +0900

    p2pcbot 알리미 20180420_01 - 가상화폐 모나코 추가.

--- registry.py
+++ registry.py
@@ -1,7 +1,7 @@
 KINDS = ('%', 'cu', 'cd')
 ALLOWED_CURRENCY = (
     'BTC', 'ETH', 'DASH', 'LTC', 'ETC', 'XRP', 'BCH', 'XMR',
-    'ZEC', 'QTUM', 'BTG', 'EOS', 'ICX', 'VEN', 'TRX', 'ELF', 'MITH'
+    'ZEC', 'QTUM', 'BTG', 'EOS', 'ICX', 'VEN', 'TRX', 'ELF', 'MITH', 'MCO'
 )
 
 class Registry:
